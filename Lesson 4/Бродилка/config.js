6369542bb6209f7340122fd8cef6f6f70684c4aa
Ilushasan <58609885+Ilushasan@users.noreply.github.com>
2020-04-20 12:44:48 +0300

    Lesson 4
    Бродилка

---
+++ Lesson 4/Бродилка/config.js
@@ -0,0 +1,9 @@
+/*
+*Объект с настройками игры.
+*@proprety {int} rowsCount Количество строк в карта.
+*@proprety {int} colsCount Количество колонок в картею
+*/
+const config = {
+    rowsCount: 10,
+    colsCount: 10,
+};
